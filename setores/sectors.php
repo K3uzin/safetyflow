4cc461dfff439aa95c786355a0adb42b843c39a3
Cleiton Almeida <120829611+K3uzin@users.noreply.github.com>
2023-10-03 20:38:12 -0300

    Create sectors.php

---
+++ setores/sectors.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html> 
+<html lang="pt-br"> 
+    <head> 
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <meta name="description" content=""> 
+        <title>Setores</title>         
+        <!-- Bootstrap core CSS -->         
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
+        <!-- Custom styles for this template -->         
+        <link rel="stylesheet" href="bootstrap/css/sectors.css">
+    </head>     
+    <body class="bg-light"> 
+        <div class="container"> 
+            <main> 
+                <div class="py-5 text-center"> 
+                    <img class="d-block mx-auto mb-4" src="img/logo.png" alt="" width="150" height="150"> 
+                    <h2>Setores</h2> 
+                </div>                 
+                <div class="row g-3"> 
+                    <div class="col-md-4"> 
+                        <div class="setor-card" style="background: #003884;"> <a href="pagina-administrativo.html" style="color: #ffffff;">Administrativo</a> 
+                        </div>                         
+                    </div>                     
+                    <div class="col-md-4"> 
+                        <div class="setor-card" style="background: #003884;"> <a href="pagina-hidro.html" style="color: #ffffff;">Hidro</a> 
+                        </div>                         
+                    </div>                     
+                    <div class="col-md-4"> 
+                        <div class="setor-card" style="background: #003884;"> <a href="pagina-cremes.html" style="color: #ffffff;">Cremes</a> 
+                        </div>                         
+                    </div>                     
+                    <div class="col-md-4"> 
+                        <div class="setor-card" style="background: #003884;"> <a href="pagina-estojos.html" style="color: #ffffff;">Estojos</a> 
+                        </div>                         
+                    </div>                     
+                    <div class="col-md-4"> 
+                        <div class="setor-card" style="background: #003884;"> <a href="pagina-qualidade.html" style="color: #ffffff;">Qualidade</a> 
+                        </div>                         
+                    </div>                     
+                    <div class="col-md-4"> 
+                        <div class="setor-card" style="background: #003884;"> <a href="pagina-logistica.html" style="color: #ffffff;">Logística</a> 
+                        </div>                         
+                    </div>                     
+                </div>                 
+            </main>             
+        </div>         
+        <footer class="my-5 pt-5 text-muted text-center text-small"> 
+            <p class="mb-1">&copy; 2023 SENAI</p> 
+            <ul class="list-inline"> 
+                <li class="list-inline-item">
+                    <a href="#">Privacidade</a> 
+                </li>                 
+                <li class="list-inline-item">
+                    <a href="#">Termos</a> 
+                </li>                 
+                <li class="list-inline-item">
+                    <a href="#">Suporte</a> 
+                </li>                 
+            </ul>             
+        </footer>         
+        <script src="assets/js/popper.min.js"></script>         
+        <script src="bootstrap/js/bootstrap.min.js"></script>         
+    </body>     
+</html>
