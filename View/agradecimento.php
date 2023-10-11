2c3c056977ca9d22baec6d970b1ed1dcb3465e60
Cleiton Almeida <120829611+K3uzin@users.noreply.github.com>
2023-10-11 19:24:56 -0300

    Create agradecimento.php

---
+++ View/agradecimento.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html> 
+<html lang="pt-br"> 
+    <head> 
+        <meta charset="utf-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <meta name="description" content=""> 
+        <title>Agradecimento</title>         
+        <!-- Bootstrap core CSS -->         
+        <link href="../Public/Css/bootstrap.min.css" rel="stylesheet"> 
+        <!-- Custom styles for this template -->         
+        <link rel="stylesheet" href="../Public/Css/thanks.css"> 
+    </head>     
+    <body>
+        <!-- Agradecimento-->
+        <main class="container text-center"> 
+            <img class="mb-1 logo" src="../Public/Css/Img/logo.png" alt="" width="150" height="150"> 
+            <h1 class="h3 mt-3">Obrigado por abrir um desvio!</h1> 
+            <p class="lead">Agradecemos pelo seu comprometimento com a segurança e melhoria contínua.</p> 
+            <p>Seu desvio foi registrado com sucesso e nossa equipe está trabalhando nele.</p> <a href="index.html" class="btn btn-primary mt-3" style="background: #003884;">Voltar à página inicial</a> 
+        </main>         
+        <script src="assets/js/popper.min.js"></script>         
+        <script src="bootstrap/js/bootstrap.min.js"></script>         
+    </body>     
+</html>
