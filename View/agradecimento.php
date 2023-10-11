38d8dddbeb686a41ae88e53739111ad7d12c1aad
Cleiton Almeida <120829611+K3uzin@users.noreply.github.com>
2023-10-11 19:38:07 -0300

    Update agradecimento.php

--- View/agradecimento.php
+++ View/agradecimento.php
@@ -13,7 +13,7 @@
     <body>
         <!-- Agradecimento-->
         <main class="container text-center"> 
-            <img class="mb-1 logo" src="../Public/Css/Img/logo.png" alt="" width="150" height="150"> 
+            <img class="mb-1 logo" src="../Public/Img/logo.png" alt="" width="150" height="150"> 
             <h1 class="h3 mt-3">Obrigado por abrir um desvio!</h1> 
             <p class="lead">Agradecemos pelo seu comprometimento com a segurança e melhoria contínua.</p> 
             <p>Seu desvio foi registrado com sucesso e nossa equipe está trabalhando nele.</p> <a href="index.html" class="btn btn-primary mt-3" style="background: #003884;">Voltar à página inicial</a> 
