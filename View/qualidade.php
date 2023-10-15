0afbbdcf1be0ee5daeac98db1ac845f583455786
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-10-15 11:21:13 -0300

    Update qualidade.php

--- View/qualidade.php
+++ View/qualidade.php
@@ -1,31 +1,42 @@
 <?php
-require '../Model/conexao.php';
+require '../Model/conexao.php'; // Verifique se o caminho está correto
 
 // Consulta para obter informações sobre o último desvio no Setor "Qualidade"
-$sql_ultimo_desvio_qualidade = "SELECT user_nome, data_identificacao, local_desvio FROM desvios WHERE setor = 5 ORDER BY id_desvio DESC LIMIT 1";
-$result_ultimo_desvio_qualidade = $mysqli->query($sql_ultimo_desvio_qualidade);
+$sql_ultimo_desvio = "SELECT usuario_matricula, data_identificacao, local_desvio FROM desvio WHERE setor_id_setor = 5 ORDER BY id_desvio DESC LIMIT 1";
+$result_ultimo_desvio = $mysqli->query($sql_ultimo_desvio);
 
-if ($result_ultimo_desvio_qualidade) {
-    $row_ultimo_desvio_qualidade = $result_ultimo_desvio_qualidade->fetch_assoc();
-    $ultimo_nome_qualidade = $row_ultimo_desvio_qualidade['user_nome'];
-    $ultimo_data_qualidade = $row_ultimo_desvio_qualidade['data_identificacao'];
-    $ultimo_local_qualidade = $row_ultimo_desvio_qualidade['local_desvio'];
+if ($result_ultimo_desvio) {
+    $row_ultimo_desvio = $result_ultimo_desvio->fetch_assoc();
+    $ultimo_matricula = $row_ultimo_desvio['usuario_matricula'];
+    $ultimo_data = $row_ultimo_desvio['data_identificacao'];
+    $ultimo_local = $row_ultimo_desvio['local_desvio'];
+
+    // Consulta para obter o nome do usuário do último desvio
+    $sql_nome_usuario = "SELECT nome FROM usuario WHERE matricula = $ultimo_matricula";
+    $result_nome_usuario = $mysqli->query($sql_nome_usuario);
+
+    if ($result_nome_usuario && $result_nome_usuario->num_rows > 0) {
+        $row_nome_usuario = $result_nome_usuario->fetch_assoc();
+        $ultimo_nome_qualidade = $row_nome_usuario['nome'];
+    } else {
+        $ultimo_nome_qualidade = "";
+    }
 } else {
-    echo "Erro na consulta do último desvio no Setor Qualidade: " . $mysqli->error;
+    echo "Erro na consulta do último desvio: " . $mysqli->error;
     $ultimo_nome_qualidade = "";
     $ultimo_data_qualidade = "";
     $ultimo_local_qualidade = "";
 }
 
 // Consulta para contar os desvios no Setor "Qualidade"
-$sql_total_desvios_qualidade = "SELECT COUNT(*) AS total_desvios_qualidade FROM desvios WHERE setor = 5";
-$result_total_desvios_qualidade = $mysqli->query($sql_total_desvios_qualidade);
+$sql_total_desvios = "SELECT COUNT(*) AS total_desvios FROM desvio WHERE setor_id_setor = 5";
+$result_total_desvios = $mysqli->query($sql_total_desvios);
 
-if ($result_total_desvios_qualidade) {
-    $row_total_desvios_qualidade = $result_total_desvios_qualidade->fetch_assoc();
-    $total_desvios_qualidade = $row_total_desvios_qualidade['total_desvios_qualidade'];
+if ($result_total_desvios) {
+    $row_total_desvios = $result_total_desvios->fetch_assoc();
+    $total_desvios_qualidade = $row_total_desvios['total_desvios'];
 } else {
-    echo "Erro na consulta do total de desvios no Setor Qualidade: " . $mysqli->error;
+    echo "Erro na consulta do total de desvios: " . $mysqli->error;
     $total_desvios_qualidade = 0;
 }
 
@@ -42,8 +53,8 @@ $mysqli->close();
     
     <h3>Último Desvio no Setor Qualidade:</h3>
     <p>Nome: <?php echo $ultimo_nome_qualidade; ?></p>
-    <p>Data: <?php echo $ultimo_data_qualidade; ?></p>
-    <p>Local: <?php echo $ultimo_local_qualidade; ?></p>
+    <p>Data: <?php echo $ultimo_data; ?></p>
+    <p>Local: <?php echo $ultimo_local; ?></p>
     
     <h3>Estatísticas de Desvios:</h3>
     <p>Total de desvios abertos no Setor Qualidade: <?php echo $total_desvios_qualidade; ?></p>
