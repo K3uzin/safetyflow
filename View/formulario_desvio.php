cced09e12388b0faec76979453df39c638db354f
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-10-16 21:14:03 -0300

    Update formulario_desvio.php

--- View/formulario_desvio.php
+++ View/formulario_desvio.php
@@ -56,7 +56,7 @@ if ($stmt_nome) {
             <div class="row g-3" style="text-align: left; align-items: center; justify-content: center;">
                 <div class="col-md-7 col-lg-8">
                     <h4 class="mb-3">Faça a diferença!</h4>
-                    <form action="../Controller/recebe_desvio.php" method="POST" style="text-align: left;">
+                    <form action="../Controller/recebe_desvio.php" method="POST" style="text-align: left;" enctype="multipart/form-data">
                         <div class="row mt-3">
                             <!-- Data de identificação do desvio -->
                             <div class="col-12">
