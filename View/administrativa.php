489413441ea16a657e48c97fdc461f78d086ea27
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-11-05 16:23:13 -0300

    Update administrativa.php

--- View/administrativa.php
+++ View/administrativa.php
@@ -5,7 +5,7 @@ require_once '../Controller/desvio.class.php';
 
 
 $desvio = new desvio;
-// Consulta para obter informações sobre o último desvio no Setor "Qualidade"
+// Consulta para obter informações sobre o último desvio no Setor "Administrativa"
 $desvio->fetch_last_desvio_setor(1,$mysqli);
 $ultimo_matricula = $desvio->get_usuario_matricula();
 $ultimo_data = $desvio->get_data_identificacao();
@@ -15,9 +15,10 @@ $sql_nome_usuario = "SELECT nome FROM usuario WHERE matricula = $ultimo_matricul
 //consulta o nome
 $result_nome_usuario = $mysqli->query($sql_nome_usuario);
 $row_nome_usuario = $result_nome_usuario->fetch_assoc();
-$ultimo_nome_qualidade = $row_nome_usuario['nome'];
-// Consulta para contar os desvios no Setor "Qualidade"
-$total_desvios_qualidade = $desvio->count_desvio_from_setor(1,$mysqli);
+$ultimo_nome_administrativa = $row_nome_usuario['nome'];
+// Consulta para contar os desvios no Setor "Administrativa"
+$total_desvios_administrativa = $desvio->count_desvio_from_setor(1,$mysqli);
+;
 
 $mysqli->close();
 ?>
@@ -31,11 +32,11 @@ $mysqli->close();
     <h2>Contador de Desvios - Setor Administrativa</h2>
     
     <h3>Último Desvio no Setor Administrativa:</h3>
-    <p>Nome: <?php echo $ultimo_nome; ?></p>
+    <p>Nome: <?php echo $ultimo_nome_administrativa; ?></p>
     <p>Data: <?php echo $ultimo_data; ?></p>
     <p>Local: <?php echo $ultimo_local; ?></p>
     
     <h3>Estatísticas de Desvios:</h3>
-    <p>Total de desvios abertos no Setor Administrativa: <?php echo $total_desvios; ?></p>
+    <p>Total de desvios abertos no Setor Administrativa: <?php echo $total_desvios_administrativa; ?></p>
 </body>
 </html>
