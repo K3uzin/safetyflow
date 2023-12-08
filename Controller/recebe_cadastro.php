e0cecac9e2154ec99163510b5426298317440e5c
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-12-08 20:55:00 -0300

    Add files via upload

--- Controller/recebe_cadastro.php
+++ Controller/recebe_cadastro.php
@@ -17,15 +17,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $isAdmin = $_POST['isAdmin'];
     $setor = $_POST['setor'];
 
+    // Verifica se a variável $_POST['area_responsavel'] está definida antes de usá-la
+    $area_responsavel = isset($_POST['area_responsavel']) ? $_POST['area_responsavel'] : null;
+
     // Cria uma instância da classe Usuario
     $novoUsuario = new Usuario();
 
     // Chama o método setUsuario para cadastrar o usuário
-    $novoUsuario->set_usuario($nome, $matricula, $email, $senha, $isAdmin, $setor, $mysqli);
+    $novoUsuario->set_usuario($nome, $matricula, $email, $senha, $isAdmin, $setor, $area_responsavel, $mysqli);
 
     // Fecha a conexão com o banco de dados
     $mysqli->close();
-    header("Location: ../cadastrar_usuario.php"); // Redireciona para a página de sucesso após o cadastro
+    header("Location: ../view/cadastrar_usuario.php"); // Redireciona para a página de sucesso após o cadastro
     exit(); // Encerra o script
 }
 ?>
