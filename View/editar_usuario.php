aec3c3b93829aa3c5b3eb3c8f5d31102a21a6694
Natan085 <119545067+Natan085@users.noreply.github.com>
2023-12-14 18:18:21 -0300

    Add files via upload

--- View/editar_usuario.php
+++ View/editar_usuario.php
@@ -19,7 +19,6 @@ if (isset($_GET['matricula'])) {
         $isAdminPost = isset($_POST['isAdmin']) ? $_POST['isAdmin'] : 0;
         $setor = $_POST['setor'];
         $area_responsavel = isset($_POST['area_responsavel']) ? $_POST['area_responsavel'] : null;
-        $status = $_POST['status'];
 
         // Verifica se a opção "não especificada" foi selecionada
         if ($area_responsavel === '') {
@@ -27,30 +26,10 @@ if (isset($_GET['matricula'])) {
             $isAdminPost = 0;
         }
 
-        // Verifica se a área já está atribuída a outro usuário apenas se o usuário for admin
-        if ($isAdminPost == 1 && $area_responsavel !== null) {
-            // Evita verificar a área se o valor for 0 (Não especificado)
-            if ($area_responsavel != 0) {
-                $queryVerificarArea = "SELECT COUNT(*) FROM usuario WHERE area_responsavel = ? AND matricula <> ?";
-                $stmtVerificarArea = $mysqli->prepare($queryVerificarArea);
-                $stmtVerificarArea->bind_param('is', $area_responsavel, $matricula);
-                $stmtVerificarArea->execute();
-                $stmtVerificarArea->bind_result($count);
-                $stmtVerificarArea->fetch();
-                $stmtVerificarArea->close();
-
-                if ($count > 0) {
-                    echo "Erro: Área já atribuída a outro usuário.";
-                    exit();
-                }
-            }
-        }
-
         // Atualiza as informações no banco de dados
-        $queryAtualizacao = "UPDATE usuario SET nome=?, email=?, senha=?, isAdmin=?, setor=?, area_responsavel=?, status=? WHERE matricula=?";
+        $queryAtualizacao = "UPDATE usuario SET nome=?, email=?, senha=?, isAdmin=?, setor=?, area_responsavel=? WHERE matricula=?";
         $stmtAtualizacao = $mysqli->prepare($queryAtualizacao);
-        $stmtAtualizacao->bind_param('sssiisss', $nome, $email, $senha, $isAdminPost, $setor, $area_responsavel, $status, $matricula);
-        
+        $stmtAtualizacao->bind_param('sssiisi', $nome, $email, $senha, $isAdminPost, $setor, $area_responsavel, $matricula);
 
         if ($stmtAtualizacao->execute()) {
             echo "Usuário atualizado com sucesso!";
@@ -64,7 +43,7 @@ if (isset($_GET['matricula'])) {
     }
 
     // Consulta as informações do usuário específico
-    $query = "SELECT matricula, nome, email, senha, isAdmin, setor, area_responsavel, status FROM usuario WHERE matricula = ?";
+    $query = "SELECT matricula, nome, email, senha, isAdmin, setor, area_responsavel FROM usuario WHERE matricula = ?";
     $stmt = $mysqli->prepare($query);
 
     // Altere 's' para 'i' se a matrícula for um número inteiro
@@ -89,45 +68,48 @@ if (isset($_GET['matricula'])) {
                     <option value='1' " . ($usuario['isAdmin'] == 1 ? 'selected' : '') . ">Sim</option>
                     <option value='0' " . ($usuario['isAdmin'] == 0 ? 'selected' : '') . ">Não</option>
                 </select><br>";
-        // Exibe o campo de seleção para a área responsável
-echo "Área Responsável: 
-<select name='area_responsavel'>
-    <option value='' " . ($usuario['area_responsavel'] === null ? 'selected' : '') . ">Escolha...</option>";
 
-$queryAreas = "SELECT id_area, nome_area FROM area_responsavel";
-$resultAreas = $mysqli->query($queryAreas);
-
-while ($rowArea = $resultAreas->fetch_assoc()) {
-echo "<option value='{$rowArea['id_area']}'";
-if ($usuario['area_responsavel'] == $rowArea['id_area']) {
-echo " selected";
-}
-echo ">{$rowArea['nome_area']}</option>";
-}
-
-echo "</select><br><br>";
-
-        // Exibe o campo para setor
+        // Exibe o campo para setor como select
         echo "Setor: 
-                <select name='setor'>
-                    <option value='1' " . ($usuario['setor'] == 1 ? 'selected' : '') . ">Administrativa</option>
-                    <option value='2' " . ($usuario['setor'] == 2 ? 'selected' : '') . ">Hidro</option>
-                    <option value='3' " . ($usuario['setor'] == 3 ? 'selected' : '') . ">Cremes</option>
-                    <option value='4' " . ($usuario['setor'] == 4 ? 'selected' : '') . ">Estojo</option>
-                    <option value='5' " . ($usuario['setor'] == 5 ? 'selected' : '') . ">Qualidade</option>
-                    <option value='6' " . ($usuario['setor'] == 6 ? 'selected' : '') . ">Logística</option>
-                </select><br>";
+                <select name='setor' required>
+                    <option value=''>Escolha...</option>";
 
-        // Exibe o campo de seleção para status
-        echo "Status: 
-                <select name='status'>
-                    <option value='ativo' " . ($usuario['status'] == 'ativo' ? 'selected' : '') . ">Ativo</option>
-                    <option value='inativo' " . ($usuario['status'] == 'inativo' ? 'selected' : '') . ">Inativo</option>
-                </select><br>";
+        $querySetores = "SELECT id_setor, nome_setor FROM setor";
+        $resultSetores = $mysqli->query($querySetores);
 
+        while ($rowSetor = $resultSetores->fetch_assoc()) {
+            echo "<option value='{$rowSetor['id_setor']}'";
+            if ($usuario['setor'] == $rowSetor['id_setor']) {
+                echo " selected";
+            }
+            echo ">{$rowSetor['nome_setor']}</option>";
+        }
+
+        echo "</select><br><br>";
+
+        // Exibe o campo de seleção para a área responsável
+        echo "Área Responsável: 
+                <select name='area_responsavel'>
+                    <option value='' " . ($usuario['area_responsavel'] === null ? 'selected' : '') . ">Escolha...</option>";
+
+        $queryAreas = "SELECT id_area, nome_area FROM area_responsavel";
+        $resultAreas = $mysqli->query($queryAreas);
+
+        while ($rowArea = $resultAreas->fetch_assoc()) {
+            echo "<option value='{$rowArea['id_area']}'";
+            if ($usuario['area_responsavel'] == $rowArea['id_area']) {
+                echo " selected";
+            }
+            echo ">{$rowArea['nome_area']}</option>";
+        }
+
+        echo "</select><br><br>";
+
+        // Exibe o botão de envio
         echo "<input type='submit' value='";
         echo ($usuario['area_responsavel'] !== null) ? 'Atualizar' : 'Salvar';
         echo "'></form>";
+
     } else {
         echo "Usuário não encontrado.";
     }
