3d06f41aa9a025677593f8c414ce25acb2abcff1
Cleiton Almeida <120829611+K3uzin@users.noreply.github.com>
2023-12-15 19:41:09 -0300

    Update administrativa.php

--- View/administrativa.php
+++ View/administrativa.php
@@ -27,6 +27,7 @@ $mysqli->close();
 <html>
 <head>
     <title>Contador de Desvios - Setor Administrativa</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Public/Css/areas.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
